7aaeda658542b155434458b598647c2ed821c840
James Willson <james.willson@att.net>
2021-07-10 13:05:33 -0500

    Added iterative max clade script

---
+++ max-clade-iter.py
@@ -0,0 +1,312 @@
+import treeswift
+import argparse
+import copy
+from tqdm import tqdm
+
+
+def get_down_clades(tree, delimiter=None):
+    """
+    Adapted from script "preproecess_multrees_v3.py" by Erin Molloy 
+    https://github.com/ekmolloy/fastmulrfs 
+
+    Gets dup free clades below each edge.
+
+    Parameters
+    ----------
+    tree : treeswift tree object
+    delimiter : delimiter separating species name from rest of leaf label
+
+    Returns list of vertices coresponding to clades without duplicates
+    """
+    clades = []
+
+    for node in tree.traverse_postorder():
+        node.dup_down = False
+        if node.is_leaf():
+            node.down = set([node.get_label().split(delimiter)[0]])
+        else:
+            node.down = set([])
+            for child in node.child_nodes():
+                expected_size = len(node.down) + len(child.down)
+                node.down = node.down.union(child.down)
+                if child.dup_down or len(node.down) < expected_size:
+                    node.dup_down = True 
+            # Only add duplication free clades to output
+            if not node.dup_down:
+                clades.append(node)
+
+    return clades
+
+
+def get_up_clades(tree):
+    """
+    Adapted from script "preproecess_multrees_v3.py" by Erin Molloy 
+    https://github.com/ekmolloy/fastmulrfs 
+
+    Gets dup free clades above each edge. 
+
+    NOTE: Must be called after 'get_down_clades'
+
+    Parameters
+    ----------
+    tree : treeswift tree object
+
+    Returns list of vertices coresponding to clades without duplicates
+    """
+
+    clades = []
+
+    # Find root node, also label rest of nodes 'skip = False'
+    for node in tree.traverse_preorder():
+        if node.is_root():
+            root = node
+            root.skip = True
+        else:
+            node.skip = False
+
+    # Check children of root
+    children_of_root = root.child_nodes()
+    for node in children_of_root:
+        node.up = set([])
+        node.skip = True
+        node.dup_up = False
+        for sibl in children_of_root:
+            if node != sibl:
+                expected_size = len(node.up) + len(sibl.down)
+                node.up = node.up.union(sibl.down)
+                if sibl.dup_down or len(node.up) < expected_size:
+                    node.dup_up = True
+        if not node.dup_up: 
+            clades.append(node)
+
+    # Check remaining nodes
+    for node in tree.traverse_preorder():
+        if not node.skip:            
+            parent = node.get_parent()
+            node.dup_up = parent.dup_up
+            node.up = parent.up
+            for sibl in parent.child_nodes():
+                if node != sibl:
+                    expected_size = len(node.up) + len(sibl.down)
+                    node.up = node.up.union(sibl.down)
+                    if sibl.dup_down or len(node.up) < expected_size:
+                        node.dup_up = True
+            if not node.dup_up:
+                clades.append(node)
+
+    return clades
+
+
+def is_max_clade(node, up):
+    """
+    Calculates whether node corresponds to maximal clade by checking all possible
+    superset clades.
+
+    NOTE: Must be run after both 'get_up_clades' and 'get_down_clades'
+
+    Parameters
+    ----------
+    node : vertex of a treeswift tree     
+    up : boolean, when true we examine edge above vertex, else below
+
+    Returns True if node corresponds to a maximal clade
+    """
+    if up:
+        for child in node.child_nodes():
+            if not child.dup_up:
+                return False
+    else:
+        parent = node.get_parent()
+        if not parent.dup_down:
+            return False
+        for sibl in parent.child_nodes():
+            if not sibl.dup_up and sibl != node:
+                return False
+
+    return True
+    
+
+def node_to_tree(tree, node, up):
+    """
+    Gets clade as tree given node
+
+    Parameters
+    ----------
+    tree : original treeswift tree
+    node : vertex of a treeswift tree     
+    up : boolean, when true clade is above node, else below
+
+    Returns tuple (clade, remainder) both treeswift trees
+    """
+    parent = node.get_parent()
+    parent.remove_child(node)
+    tree.suppress_unifurcations()
+
+    clade = tree.extract_subtree(node)
+    clade.suppress_unifurcations()
+    # c_tree = treeswift.read_tree_newick(tree.newick())
+    # c_tree.reroot(node) 
+    # t2 = c_tree.extract_subtree(parent)
+    # t2.suppress_unifurcations()
+    return (clade, tree) if up else (tree, clade)
+    # if up:
+    #     parent = node.get_parent()
+    #     c_tree = copy.copy(tree)
+    #     c_tree.reroot(node)
+    #     return c_tree.extract_subtree(parent)
+    # else:
+    #     return tree.extract_subtree(node)
+
+
+def find_max_clades(tree, delimiter=None):
+    """
+    Find all the max clades in a tree.
+
+    Parameters
+    ----------
+    tree : treeswift tree object
+    delimiter : delimiter separating species name from rest of leaf label
+
+    Returns list containing all clades as treeswift tree objects
+    """
+    down_clades = get_down_clades(tree, delimiter)
+    up_clades = get_up_clades(tree)
+
+    max_clade, max_size, up = None, -1, False
+    # if there's no duplication events below the root, then there's no duplications in tree
+    if not tree.root.dup_down:
+        return tree, None
+    else:
+        for clade in down_clades:
+            if len(clade.down) > max_size:
+                max_size = len(clade.down)
+                max_clade = clade
+        for clade in up_clades:
+            if len(clade.up) > max_size:
+                max_size = len(clade.up)
+                max_clade = clade
+                up = True
+        # for clade in down_clades:
+        #     if is_max_clade(clade, False):
+        #         max_clades.append(node_to_tree(tree, clade, False))
+        # for clade in up_clades:
+        #     if is_max_clade(clade, True):
+        #         max_clades.append(node_to_tree(tree, clade, True))
+        if max_clade is None:
+            return None, None
+
+        clade_tree = tree.extract_subtree(max_clade)
+        clade_tree.suppress_unifurcations()
+
+        parent = max_clade.get_parent()
+        parent.remove_child(max_clade)
+        tree.suppress_unifurcations()
+        
+        return (clade_tree, tree) if not up else (tree, clade_tree)
+
+
+def trivial(newick_str):
+    """
+    Determines if a newick string represents a trivial tree (tree containing no quartets).
+
+    Parameters
+    ----------
+    newick_str: newick string
+
+    Returns True if tree contains less than two '('
+    """
+    count = 0
+    for c in newick_str:
+        if c == '(':
+            count += 1
+        if count > 1:
+            return False
+    return True
+
+
+def unroot(tree):
+    """
+    Unroots treeswift tree. Adapted from treeswift 'deroot' function.
+    This one doesn't contract (A,B); to A;
+
+    Parameters
+    ----------
+    tree: treeswift tree
+
+    Returns unrooted treeswift tree
+    """
+    if tree.root.num_children() == 2:
+        [left, right] = tree.root.child_nodes()
+        if not right.is_leaf():
+            right.contract()
+        elif not left.is_leaf():
+            left.contract()
+    tree.is_rooted = False
+    return tree
+
+def is_single_copy(tree, delimiter=None):
+    """
+    Detects if treeswift tree is a single-copy tree.
+
+    Parameters
+    ----------
+    tree: treeswift tree
+    delimiter: character separating species name from rest of leaf label
+
+    Returns true if tree is single copy
+    """
+    return tree.num_nodes(internal=False) == len(set(l.label.split(delimiter)[0] for l in tree.traverse_postorder(internal=False)))
+
+def main(args):
+    if args.output is None:
+        split = args.input.rsplit('.', 1)
+        output = split[0] + '-mclades.' + split[1]
+    else:
+        output = args.output
+    
+    num_trees = sum([1 for _ in open(args.input, 'r')])
+    
+    with open(args.input, 'r') as fi, open(output, 'w') as fo:
+        for line in tqdm(fi, total=num_trees):
+            tree = treeswift.read_tree_newick(line)
+            out = []
+            while True:
+                unroot(tree)
+                max_clade, remainder = find_max_clades(tree, args.delimiter)
+                if max_clade is None:
+                    break
+                # get largest max-clade
+                #max_clades.sort(key=lambda x:x[0].num_nodes(internal=False), reverse=True)
+                #out.append(max_clades[0][0])
+                out.append(max_clade)
+                # delete from tree --- then iterate
+                # if max_clades[0][1] is None:
+                #     break
+                if remainder is None:
+                    break
+                tree = treeswift.read_tree_newick(remainder.newick())
+                #if tree is None: break
+            #out.append(tree) # append remaining single-copy tree
+
+            for c in out:
+                unroot(c)
+                c.suppress_unifurcations()
+                newk = c.newick()
+                if args.trivial or not trivial(newk):
+                    fo.write(newk + '\n')
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i", "--input", type=str,
+                        help="Input tree list file", required=True)
+    parser.add_argument("-o", "--output", type=str,
+                        help="Output max clade list file")
+    parser.add_argument("-t", "--trivial", action='store_true', 
+                        help="Include trivial clades in output")
+    parser.add_argument('-d', "--delimiter", type=str, 
+                        help="Delimiter separating species name from rest of leaf label.")
+
+    main(parser.parse_args())
+    
